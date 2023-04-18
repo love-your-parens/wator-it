d58bddc53c91bf06111910c35a087e496848ac02
Konrad Wątor <konrad@wator.it>
2023-04-18 03:09:57 +0200

    Add Tailwind adjustments & extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ module.exports = {
     "layouts/**/*.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'two-face-light': "url('/img/bg-photo.png')",
+        'two-face-dark': "url('/img/bg-photo.png')",
+        'two-face-outline': "url('/img/face-outlines.svg')",
+      }
+    },
+    screens: {
+      '2xl': '1440px',
+    },
+    keyframes: {
+      shimmer: {
+        '0%, 100%': {
+          transform: 'translateX(5px) translateY(-5px)',
+        },
+        '50%': {
+          transform: 'translateX(-5px) translateY(5px)',
+        },
+      }
+    },
+    animation: {
+      shimmer: 'shimmer 9s ease-in-out infinite',
+    }
   },
   plugins: [],
   darkMode: 'class',
