9a92daa6e53cea01fd39709a3b6c6d4f81403d7f
Konrad Wątor <konrad@wator.it>
2023-04-23 04:37:56 +0200

    Incorporate column mixins

--- hiccup/layouts/_default/single.clj
+++ hiccup/layouts/_default/single.clj
@@ -1,16 +1,12 @@
-(require '[hugo :refer [raw-tag -raw-tag-] :rename {raw-tag t -raw-tag- -t-}])
+(require '[hugo :refer [raw-tag -raw-tag-] :rename {raw-tag t -raw-tag- -t-}]
+         '[mixins :refer [left-column right-column]])
 
 (list
  (-t- define "main")
- [:article {:class ["bg-neutral-100" "dark:bg-neutral-900"
-                    "w-3/4" "p-5" "lg:w-1/2"
-                    "min-h-[calc(100vh)]"]}
-  [:h1 (t .Title)] (t .Content)] 
+ (left-column [:article {:class ["min-h-[90vh]"]}
+               [:h1 (t .Title)]
+               (t .Content)])
  (-t- end)
  (t define "footer")
- [:div {:class ["bg-neutral-100" "dark:bg-neutral-900"
-                "p-1" "w-3/4" "mt-2"
-                "lg:w-1/2" "lg:ml-[50%]" "lg:mt-0"
-                "text-right"]}
-  (t partial "footer" .)]
+ (right-column [:footer (t partial "footer" .)])
  (t end))
