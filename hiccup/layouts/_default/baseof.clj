3223951f5f2ed9dffd242fb0c3d8296b5056a75a
Konrad Wątor <konrad@wator.it>
2023-04-23 04:39:02 +0200

    Improve responsiveness

--- hiccup/layouts/_default/baseof.clj
+++ hiccup/layouts/_default/baseof.clj
@@ -2,7 +2,7 @@
          '[hugo :refer [raw-tag] :rename {raw-tag t}]
          '[mixins :refer [feather]])
 
-(list 
+(list
  (raw "<!DOCTYPE html>")
  (raw "<html lang='{{ site.Language.Lang | default \"en\" }}'>")
  [:head
@@ -13,38 +13,36 @@
   (t $bg2 := resources.Get "img/face-outlines.svg") (t $bg2.RelPermalink)
   (t $bg3 := resources.Get "img/feather.svg")]
  [:body {:id "bottom-layer"
-         :class ["bg-neutral-400" "text-neutral-800"
+         :class ["bg-neutral-400" "text-neutral-800" "font-serif"
                  "dark:bg-neutral-700" "dark:text-neutral-200"
                  "max-w-screen-2xl" "m-auto" "transition-colors"]}
   (let [sizing ["fixed" "w-screen" "h-screen" "bottom-0" "-z-10"]
         bg ["bg-fixed" "bg-no-repeat" "bg-contain"]]
     [:div {:id "intermediate-layers"}
      [:div {:class (concat sizing bg
-                           ["bg-two-face-light" "dark:bg-two-face-dark"
+                           ["panoramic:hidden"
+                            "bg-two-face-light" "dark:bg-two-face-dark"
                             "landscape:bg-[right_-50vh_bottom_-25vh]"
-                            "portrait:bg-[right_-50vw_bottom]"
-                            "lg:landscape:bg-bottom"
-                            "lg:portrait:bg-bottom"])}]
+                            "portrait:bg-[right_-50vw_bottom_-7vh]"
+                            "lg:landscape:bg-[center_bottom_-12vh]"
+                            "lg:portrait:bg-[center_bottom_-5vh]"])}]
      [:div {:class (concat sizing bg
-                           ["bg-two-face-outline"
+                           ["panoramic:hidden"
+                            "bg-two-face-outline"
                             "landscape:bg-[right_-50vh_bottom_-25vh]"
-                            "portrait:bg-[right_-50vw_bottom]"
-                            "lg:landscape:bg-bottom"
-                            "lg:portrait:bg-bottom"])}]
+                            "portrait:bg-[right_-50vw_bottom_-7vh]"
+                            "lg:landscape:bg-[center_bottom_-12vh]"
+                            "lg:portrait:bg-[center_bottom_-5vh]"])}]
      [:div {:class (concat sizing bg
                            ["lg:hidden"
                             "dark:bg-two-face-dark" "bg-two-face-light"
                             "landscape:bg-[left_-50vh_bottom_-25vh]"
-                            "portrait:bg-[left_-50vw_bottom]"
-                            "lg:landscape:bg-bottom"
-                            "lg:portrait:bg-bottom"])}]
+                            "portrait:bg-[left_-50vw_bottom_-7vh]"])}]
      [:div {:class (concat sizing bg
                            ["lg:hidden"
                             "bg-two-face-outline"
                             "landscape:bg-[left_-50vh_bottom_-25vh]"
-                            "portrait:bg-[left_-50vw_bottom]"
-                            "lg:landscape:bg-bottom"
-                            "lg:portrait:bg-bottom"])}]
+                            "portrait:bg-[left_-50vw_bottom_-7vh]"])}]
      [:div {:class (into sizing ["opacity-90" "flex" "justify-evenly" "items-start" "px-8"])}
       [:span {:class ["feather-fall" "feather-fall--delay-2"]}
        (feather {:size 32 :rotation 180 :stroke-width ".5" :outer-color "#6c65ca"})]
@@ -59,8 +57,7 @@
       [:span {:class ["feather-fall" "feather-fall--delay-2"]}
        (feather {:size 32 :rotation 180 :stroke-width ".5" :outer-color "#f00f74"})]]])
   [:div {:id "top-layer"}
-   [:div {:id "top-bar" :class ["mt-4"]}
-    (t partial "header" .)]
-   [:main (t block "main" .) (t end)]]
-  [:footer (t block "footer" .) (t end)]]
+   [:header (t partial "header" .)]
+   [:main (t block "main" .) (t end)]
+   [:footer (t block "footer" .) (t end)]]]
  (raw "</html>"))
