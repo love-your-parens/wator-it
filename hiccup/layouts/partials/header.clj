0e0c27a00e4c493a02ab7c25b3e9755bdd2c0bba
Konrad Wątor <konrad@wator.it>
2023-04-23 04:40:13 +0200

    Refashion the top section

--- hiccup/layouts/partials/header.clj
+++ hiccup/layouts/partials/header.clj
@@ -1,9 +1,11 @@
 (require '[hugo :as h :refer [raw-tag] :rename {raw-tag t}])
 
-[:header {:class ["px-3" "md:px-5" "py-4" "flex" "justify-between" "items-baseline"]}
- [:div
-  [:div {:class ["inline-block" "font-serif"]} (t partial "emblem" .)]
-  [:div {:class ["inline-block" "ml-5"]} (t partial "navigation" .)]]
- [:div
-  [:div {:class ["inline-block"]} (t partial "language-selection" .)]
-  [:div {:class ["inline-block"]} (t partial "dark-mode-switch" .)]]]
+[:section {:class ["m-1"]}
+ [:div {:class ["flex" "justify-between" "items-baseline" "font-sans"]}
+  [:nav {:class ["flex"]}
+   (t partial "navigation-nested" .Site.Menus.main)]
+  [:div {:class ["flex"]}
+   (t partial "language-selection" .)
+   (t partial "dark-mode-switch" .)]]
+ [:div {:class ["text-center" "my-4"]}
+  (t partial "emblem" .)]]
--- hiccup/layouts/partials/navigation.clj
+++
@@ -1,4 +0,0 @@
-(require '[hugo :refer [raw-tag] :rename {raw-tag t}])
-
-[:nav {:class ["flex space-x-2"]}
- (t partial "navigation-nested" .Site.Menus.main)]
