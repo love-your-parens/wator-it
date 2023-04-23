974589d1e00bbbda25034a07249d4863d960cc1e
Konrad Wątor <konrad@wator.it>
2023-04-23 04:35:10 +0200

    Add fonts and media dimensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,28 +4,25 @@ module.exports = {
     "themes/konrad/layouts/**/*.html",
     "layouts/**/*.html",
   ],
+  plugins: [],
+  darkMode: 'class',
   theme: {
     extend: {
       backgroundImage: {
         'two-face-light': "url('/img/bg-photo.png')",
         'two-face-dark': "url('/img/bg-photo.png')",
         'two-face-outline': "url('/img/face-outlines.svg')",
-      }
-    },
-    keyframes: {
-      shimmer: {
-        '0%, 100%': {
-          transform: 'translateX(5px) translateY(-5px)',
+      },
+      fontFamily: {
+        sans: '"Montserrat", sans-serif',
+        serif: '"Hepta Slab", serif',
+        display: '"Poiret One", cursive',
+      },
+      screens: {
+        'panoramic': {
+          'raw': '(min-width: 1024px) and (max-height: 400px)',
         },
-        '50%': {
-          transform: 'translateX(-5px) translateY(5px)',
-        },
-      }
+      },
     },
-    animation: {
-      shimmer: 'shimmer 9s ease-in-out infinite',
-    }
   },
-  plugins: [],
-  darkMode: 'class',
 }
