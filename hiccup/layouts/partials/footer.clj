5e5299e1fd4a4b18cf17f0b8d8326724e76e8fb8
Konrad Wątor <konrad@wator.it>
2023-05-02 22:29:10 +0200

    Update the footer

--- hiccup/layouts/partials/footer.clj
+++ hiccup/layouts/partials/footer.clj
@@ -1,8 +1,8 @@
 (require '[hiccup.util :refer [raw-string] :rename {raw-string r}]
-         '[hugo :refer [tag] :rename {tag t}])
+         '[hugo :refer [raw-tag] :rename {raw-tag t}])
 
 [:a {:href (t .Site.BaseURL)
-     :class []}
+     :class ["font-sans" "font-black"]}
  (r "&copy;"
     (t now.Format "2006")
     " " (t .Site.Copyright | default .Site.Author | safeHTML))]
