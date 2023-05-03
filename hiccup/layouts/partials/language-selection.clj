035c4a48f7b565a312546e77b53fde89e4ed3e1d
Konrad Wątor <konrad@wator.it>
2023-05-04 00:22:05 +0200

    Redesign the language selection menu

--- hiccup/layouts/partials/language-selection.clj
+++ hiccup/layouts/partials/language-selection.clj
@@ -1,9 +1,9 @@
 (require '[hugo :as h :refer [raw-tag raw-tag-] :rename {raw-tag t raw-tag- t-}])
 
-[:ul
+(list 
  (t- range .Translations)
- [:li
-  [:a {:href (t .Permalink)}
-   (t .Lang)
-   (t replace .Lang "en" "gb" | printf ":flag_%s:" | emojify)]]
- (t- end)]
+ [:a {:href (t .RelPermalink) :class ["flex" "items-center" "hover:underline"]} 
+  (t .Site.Language.LanguageName)
+  (t $flag := resources.Get (printf "img/flag-%s.svg" .Lang))
+  [:img {:src (t $flag.RelPermalink) :width 36 :class ["mx-1"]}]]
+ (t- end))
