bdfa68fa07ef10cce37d90fd89e8d5950e17906c
Konrad Wątor <konrad@wator.it>
2023-05-04 00:25:34 +0200

    Update the index page

--- hiccup/layouts/index.pl.clj
+++ hiccup/layouts/index.pl.clj
@@ -1,8 +1,8 @@
 (require '[hugo :refer [raw-tag] :rename {raw-tag t}]
-         '[mixins :refer [link left-column right-column feather-footer]])
+         '[mixins :refer [external-link left-column right-column feather-footer]])
 
 (list
- 
+
  (t define "main")
  [:div {:class ["mx-auto" "text-sm" "md:text-base"]}
   (left-column
@@ -20,43 +20,45 @@
   (right-column
    [:article
     [:h1 "Wszystko jest dla ludzi"]
-    [:p "Z tego, co przygotowuję ma być pożytek. Nie wystarczy, że działa. " 
-     "Musi być wygodne w obsłudze; Musi być czytelne. A i oczu razić nie powinno. " 
+    [:p "Z tego, co przygotowuję ma być pożytek. Nie wystarczy, że działa. "
+     "Musi być wygodne w obsłudze; Musi być czytelne. A i oczu razić nie powinno. "
      "Słowem: kładę nacisk na "
      [:em [:abbr {:title "User Experience"} "UX"]]
      " i nie lekceważę niczego, co przekłada się na odczucia odbiorcy."]
-    [:p "Schludna powierzchowność to nie wszystko. Nie ma programów skończonych. Większość przechodzi z rąk do rąk. " 
+    [:p "Schludna powierzchowność to nie wszystko. Nie ma programów skończonych. Większość przechodzi z rąk do rąk. "
      "Dbam zatem, aby moją pracę mogli podjąć, lub spożytkować inni, nie głowiąc się przy tym stale - co też autor miał na myśli. "
      "Dobrze napisany kod mówi ponoć sam za siebie, ale nie mówi wszystkiego. Wymaga didaskaliów."]
     (feather-footer)])
   (left-column
    [:article [:h1 "Za pazuchą"]
-    [:p "W pracy korzystam z wielu narzędzi. Dobieram je pragmatycznie. " 
-     "Lubię co prawda rozwiązania schludne, ale zdaje sobie sprawę, że trzeba czasem odpuścić i sięgnąć po " 
+    [:p "W pracy korzystam z wielu narzędzi, które dobieram pragmatycznie. "
+     "Lubię co prawda rozwiązania schludne, ale zdaje sobie sprawę, że trzeba czasem odpuścić i sięgnąć po "
      [:em "większy młotek"] ". "]
-    [:h3 "Wybrane narzędzia"] 
+    [:h3 "Wybrane narzędzia"]
     [:ul {:class ["list-inside" "list-disc"]}
-     [:li (interpose " / " [(link "https://php.net" "PHP")
-                            (link "https://symfony.com/" "Symfony")])]
-     [:li (interpose " / " [(link "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"
-                                  "Javascript")
-                            (link "https://www.typescriptlang.org/" "TypeScript")
-                            (link "https://jquery.com/" "jQuery")])]
-     [:li (interpose " / " [(link "https://clojure.org/" "Clojure")
-                            (link "https://clojurescript.org/" "ClojureScript")
-                            (link "https://babashka.org/" "Babashka")])]
-     [:li (interpose " / " [(link "https://python.org/" "Python")
-                            (link "https://bottlepy.org" "Bottle")
-                            (link "https://www.djangoproject.com/" "Django")])]
-     [:li (interpose " / " [(link "https://www.w3.org/TR/CSS/#css" "CSS")
-                            (link "https://sass-lang.com/" "SASS")
-                            (link "https://tailwindcss.com" "Tailwind")])]
-     [:li (interpose " / " [(link "https://www.mysql.com/" "MySQL")
-                            (link "https://mariadb.org/" "MariaDB")])]
-     [:li (interpose " / " [(link "https://www.docker.com/" "Docker")])]]
-
-    (feather-footer)])]
+     [:li (interpose " / " [(external-link "https://php.net" "PHP")
+                            (external-link "https://symfony.com/" "Symfony")])]
+     [:li (interpose " / " [(external-link "https://www.ecma-international.org/publications-and-standards/standards/ecma-262/"
+                                           "Javascript")
+                            (external-link "https://www.typescriptlang.org/" "TypeScript")
+                            (external-link "https://jquery.com/" "jQuery")])]
+     [:li (interpose " / " [(external-link "https://clojure.org/" "Clojure")
+                            (external-link "https://clojurescript.org/" "ClojureScript")
+                            (external-link "https://babashka.org/" "Babashka")])]
+     [:li (interpose " / " [(external-link "https://python.org/" "Python")
+                            (external-link "https://bottlepy.org" "Bottle")
+                            (external-link "https://www.djangoproject.com/" "Django")])]
+     [:li (interpose " / " [(external-link "https://www.w3.org/TR/CSS/#css" "CSS")
+                            (external-link "https://sass-lang.com/" "SASS")
+                            (external-link "https://tailwindcss.com" "Tailwind")])]
+     [:li (interpose " / " [(external-link "https://www.mysql.com/" "MySQL")
+                            (external-link "https://mariadb.org/" "MariaDB")])]
+     [:li (interpose " / " [(external-link "https://www.docker.com/" "Docker")])]]
+    (feather-footer)])
+  (right-column
+   [:article [:h1 {:id "contact"} "Kontakt"]
+    [:p]])]
  (t end)
  (t define "footer")
- (right-column [:div (t partial "footer" .)])
+ (left-column [:div (t partial "footer" .)])
  (t end))
