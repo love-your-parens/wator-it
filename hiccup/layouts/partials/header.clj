118d3b81860242b94645604ebb81b96e3e32c529
Konrad Wątor <konrad@wator.it>
2023-05-04 00:23:25 +0200

    Reorganise the header section

--- hiccup/layouts/partials/header.clj
+++ hiccup/layouts/partials/header.clj
@@ -2,10 +2,10 @@
 
 [:section {:class ["m-1"]}
  [:div {:class ["flex" "justify-between" "items-baseline" "font-sans"]}
-  [:nav {:class ["flex"]}
-   (t partial "navigation-nested" .Site.Menus.main)]
-  [:div {:class ["flex"]}
-   (t partial "language-selection" .)
-   (t partial "dark-mode-switch" .)]]
+  [:nav {:class ["mx-1"]} [:a {:href "#contact" :class ["hover:underline"]} (t i18n "contact")]]
+  [:nav {:class ["flex"]} (t partial "navigation-nested" .Site.Menus.main)]
+  [:div {:class ["flex" "items-center"]}
+   [:div {:class ["mx-2"]} (t partial "language-selection" .)]
+   [:div {:class ["mx-1"]} (t partial "dark-mode-switch" .)]]]
  [:div {:class ["text-center" "my-4"]}
   (t partial "emblem" .)]]
