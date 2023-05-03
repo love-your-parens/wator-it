6e68b8a92641f6ce30aae231db4d6fdd9af7290e
Konrad Wątor <konrad@wator.it>
2023-05-04 00:21:00 +0200

    Redesign the dark-mode control switch

--- hiccup/layouts/partials/dark-mode-switch.clj
+++ hiccup/layouts/partials/dark-mode-switch.clj
@@ -1,6 +1,12 @@
+(require '[hugo :refer [raw-tag] :rename {raw-tag t}])
+
 (list
- [:label
-  {:class ["dark-mode-controls" "cursor-pointer" "select-none"]}
+ [:label.dark-mode-controls
+  {:class ["block" "cursor-pointer" "select-none" "h-[36px]" "overflow-hidden"]}
   [:input.dark-mode-toggle.hidden {:type :checkbox}]
-  [:span]]
+  (t $sun := resources.Get ("img/sun.svg"))
+  (t $moon := resources.Get ("img/moon.svg"))
+  [:div {:class ["relative" "transition-all" "hover:-mt-[36px]" "dark:-mt-[36px]" "dark:hover:mt-0"]}
+   [:img {:src (t $sun.RelPermalink) :width 36 :class ["bg-neutral-200" "rounded-full" "p-[3px]"]}]
+   [:img {:src (t $moon.RelPermalink) :width 36 :class ["bg-neutral-200" "rounded-full" "p-[2px]"]}]]]
  [:script "document.darkModeSwitchesPresent = true"])
