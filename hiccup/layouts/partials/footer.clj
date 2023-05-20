5dd84f5903099ced41850df637bc5df10874d5a1
Konrad Wątor <konrad@wator.it>
2023-05-20 20:24:08 +0200

    Remove footer hyperlink

--- hiccup/layouts/partials/footer.clj
+++ hiccup/layouts/partials/footer.clj
@@ -1,8 +1,7 @@
 (require '[hiccup.util :refer [raw-string] :rename {raw-string r}]
          '[hugo :refer [raw-tag] :rename {raw-tag t}])
 
-[:a {:href (t .Site.BaseURL)
-     :class ["font-sans" "font-black"]}
+[:span {:class ["font-sans" "font-black"]}
  (r "&copy;"
     (t now.Format "2006")
     " " (t .Site.Copyright | default .Site.Author | safeHTML))]
